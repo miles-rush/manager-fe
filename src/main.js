21b871bfdf5cabc8a325b5371cd073f8748e6de1
KingRainGrey <32055007+miles-rush@users.noreply.github.com>
2022-07-01 15:41:32 +0800

    storage二次封装

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import request from './util/request'
+import storage from './util/storage'
 
 
 console.log('环境变量=>',import.meta.env)
 const app = createApp(App);
-app.config.globalProperties.$request = request
+app.config.globalProperties.$request = request;
+app.config.globalProperties.$storage = storage;
 app.use(ElementPlus).use(router).mount('#app')
